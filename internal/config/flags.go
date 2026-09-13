f123243079dcc8d7ebe9ae3cecd9eda3c537e34e
Jaren Glenn <jarenglenn@pm.me>
2026-09-13 13:55:10 -0600

    feat(config): config/flag now has support for multiple types

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -14,11 +14,21 @@ func Flags() []cli.Flag {
 			return
 		}
 
-		flags = append(flags, &cli.StringFlag{
-			Name:  f.Key,
-			Usage: f.Usage,
-		})
+		flags = append(flags, newFlag(f))
 	})
 
 	return flags
+}
+
+func newFlag(f schemaField) cli.Flag {
+	switch f.Type {
+	case fieldTypeStrings:
+		return &cli.StringSliceFlag{Name: f.Key, Usage: f.Usage}
+	case fieldTypeInt:
+		return &cli.IntFlag{Name: f.Key, Usage: f.Usage}
+	case fieldTypeBool:
+		return &cli.BoolFlag{Name: f.Key, Usage: f.Usage}
+	default:
+		return &cli.StringFlag{Name: f.Key, Usage: f.Usage}
+	}
 }
