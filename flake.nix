5abbfd194efee68abf790f70b9cf1b3bc55dd092
Jaren Glenn <jarenglenn@pm.me>
2026-09-13 18:20:22 -0600

    chore: bump version to 0.3.0

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
       pname = "mise";
       src = ./.;
       vendorHash = "sha256-7ziKWtC0GhGXeE7DiPfI8aAOSch/Scrv5Hcc/KyO7jI=";
-      version = "0.2.2";
+      version = "0.3.0";
     };
 
     mkShell = ollama:
